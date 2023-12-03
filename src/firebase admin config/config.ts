df80dc1173e86a12ef58e321655c8348fb4fd7cf
Ace G <aceguevarra48@gmail.com>
2023-12-03 22:48:37 +0800

    Fixed

--- src/firebase admin config/config.ts
+++ src/firebase admin config/config.ts
@@ -1,10 +1,10 @@
-import admin from "firebase-admin";
-import { initializeApp } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import env from "../utils/envalid";
+// import admin from "firebase-admin";
+// import { initializeApp } from "firebase-admin/app";
+// import { getAuth } from "firebase-admin/auth";
+// import env from "../utils/envalid";
 
-const app = initializeApp({
-  credential: admin.credential.cert(env.GOOGLE_APPLICATION_CREDENTIALS),
-});
+// const app = initializeApp({
+//   credential: admin.credential.cert(env.GOOGLE_APPLICATION_CREDENTIALS),
+// });
 
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
