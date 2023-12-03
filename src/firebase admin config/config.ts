61dc52fd9533fbb5f9fcf3b24f246f8f9e366d4f
Ace G <aceguevarra48@gmail.com>
2023-12-03 23:23:25 +0800

    Fixy

--- src/firebase admin config/config.ts
+++ src/firebase admin config/config.ts
@@ -1,10 +1,11 @@
 import admin from "firebase-admin";
 import { initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import applicationDetails from "./igotyou-399523-firebase-adminsdk-dape5-6a0d653f21.json";
 
 const app = initializeApp({
-  credential: admin.credential.cert(JSON.stringify(applicationDetails)),
+  credential: admin.credential.cert(
+    "C:/Users/Administrator01/Downloads/igotyou-399523-firebase-adminsdk-dape5-6a0d653f21.json"
+  ),
 });
 
 export const auth = getAuth(app);
