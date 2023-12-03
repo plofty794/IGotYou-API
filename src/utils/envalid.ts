babff43dee0ed42e45173114a032f07a6f2fdf2d
Ace G <aceguevarra48@gmail.com>
2023-12-03 22:52:51 +0800

    Fixed

--- src/utils/envalid.ts
+++ src/utils/envalid.ts
@@ -7,7 +7,6 @@ export default cleanEnv(process.env, {
   MONGO_COMPASS_URI: str(),
   PORT: port(),
   SALT: num(),
-  GOOGLE_APPLICATION_CREDENTIALS: str(),
   CLOUDINARY_CLOUD_NAME: str(),
   CLOUDINARY_API_KEY: str(),
   CLOUDINARY_API_SECRET: str(),
