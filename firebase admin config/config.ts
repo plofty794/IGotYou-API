ffbb4a6c7abf45d453f89fe23e5d3bdbcffdce7a
Ace G <aceguevarra48@gmail.com>
2024-02-08 09:50:28 +0800

    Configured json file path parsing

--- firebase admin config/config.ts
+++ firebase admin config/config.ts
@@ -2,10 +2,11 @@ import admin from "firebase-admin";
 import { initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import env from "../utils/envalid";
+import path from "path";
 
 const app = initializeApp({
   credential: admin.credential.cert(
-    env.FIREBASE_SERVICE_KEY.replace(/\\n/g, "\n")
+    path.resolve(env.FIREBASE_SERVICE_KEY.replace(/\\n/g, "\n"))
   ),
 });
 
