69a2ce7f0447b2704f2236b9bf6cda1bcbd0424a
Ace G <aceguevarra48@gmail.com>
2024-02-08 21:37:41 +0800

    A

--- firebase admin config/config.ts
+++ firebase admin config/config.ts
@@ -5,9 +5,12 @@ import env from "../utils/envalid";
 import path from "path";
 
 const app = initializeApp({
-  credential: admin.credential.cert(
-    path.resolve(env.FIREBASE_SERVICE_KEY.replace(/\\n/g, "\n"))
-  ),
+  credential: admin.credential.cert({
+    clientEmail:
+      "firebase-adminsdk-dape5@igotyou-399523.iam.gserviceaccount.com",
+    privateKey: "6a0d653f2140e295eecae9fd97da268f8ebe9400",
+    projectId: "igotyou-399523",
+  }),
 });
 
 export const auth = getAuth(app);
